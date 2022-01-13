fe1fccc82f3fddc25aab546bd0bed89969b4578a
Iryna Lyshenko <i.lyshenko@brainence.com>
2022-01-13 13:32:05 +0200

    init

---
+++ test-component/placeholder.js
@@ -0,0 +1,54 @@
+"use strict"
+
+define(function (require) {
+    const placeholderManager = require("core/placeholderManager");
+
+    var docDefinition;
+
+    var placeHolder = function ($scope, $element, controlService) {
+
+        //const _this = this;
+        this.getItems = () => {
+            var items = [{
+                text: "Test Button",  // Button name
+                key: "placeholderTestButton",  // Button id (unique)
+                icon: "fa fa-angle-double-up",  // Button icon
+                content: {
+                    moduleName: "placeholderTestButtonTemplate",
+                    controlName: "placeholderTestButtonTemplate"
+                }
+            }];
+
+            return items;
+        };
+
+        this.isEnabled = (itemKey) => {
+            return true;
+        };
+
+        this.onClick = () => {
+            var orders = $scope.viewStats.get_selected_orders();
+
+            if (orders.length < 1) {
+                alert('Please select at least one order');
+                return;
+            }
+            
+            var ids = [];
+            for (var i = 0; i < orders.length; i++)
+            {
+                var id = orders[i].id;
+                ids.push(id);
+            }
+            
+            alert(ids.length);
+            console.log(ids);
+        };
+
+       
+    };
+      
+
+    placeholderManager.register("OpenOrders_OrderControlButtons", placeHolder);
+
+});
